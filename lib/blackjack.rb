d9fd2b07f7d93607b97738ee8b533d23a388f9c1
Kolton Starr <starrkolton@gmail.com>
2019-04-27 02:22:43 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,43 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  return card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  x = deal_card + deal_card
+  display_card_total(x)
 end
 
-def hit?
-  # code hit? here
-end
-
-def invalid_command
-  # code invalid_command here
+def hit?(card_total)
+  prompt_user
+  x = get_user_input
+  if (x == "h")
+    y = card_total + deal_card
+  elsif (x == "s") 
+    card_total 
+  else 
+    hit?(card_total)
+  end
 end
 
 #####################################################
@@ -39,6 +45,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  y = initial_round
+  x = hit?(y)
+  display_card_total(x)
+  if x > 21 
+    end_game(x)
+  else 
+    puts "Your cards add up to #{x}"
+    x
+  end
 end
     
